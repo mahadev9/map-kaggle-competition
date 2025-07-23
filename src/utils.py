01826318d81d355243fb5d8f1b39bb090559cce4
Mahadev Maitri <mahadevmaitri@gmail.com>
2025-07-22 22:15:42 -0400

    updates

--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,101 @@
+import os
+
 import torch
 import numpy as np
+from typing import Dict
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+)
 
 
-def stringify_input(row):
+def get_model_name(is_kaggle, root_path) -> str:
+    if is_kaggle:
+        return os.path.join(
+            root_path,
+            "microsoftdeberta-v3-small/transformers/default/1/deberta-v3-small",
+        )
+    return "microsoft/deberta-v3-xsmall"
+
+
+def stringify_input(row) -> str:
     output = (
-        f"Question: {row['QuestionText']}\n"
-        f"Answer: {row['MC_Answer']}\n"
-        f"Student Explanation: {row['StudentExplanation']}\n\n"
+        f"Math Question: {row['QuestionText']}\n"
+        f"Student's Answer: {row['MC_Answer']}\n"
+        f"Student's Explanation: {row['StudentExplanation']}\n"
     )
     if "is_mc_answer_correct" in row:
-        output += f"Is the student's answer correct? {row['is_mc_answer_correct']}\n"
+        output += f"Answer Correctness: {'Correct' if row['is_mc_answer_correct'] else 'Incorrect'}\n"
     if "is_student_explanation_correct" in row:
-        output += f"Is the student's explanation correct? {row['is_student_explanation_correct']}\n"
+        output += f"Explanation Correctness: {'Correct' if row['is_student_explanation_correct'] else 'Incorrect'}\n"
+
+    output += "\nTask: Identify the student's misconception category and specific misconception."
     return output.strip()
 
 
-def compute_map3(eval_pred):
+def compute_map3(eval_pred) -> Dict[str, float]:
     logits, labels = eval_pred
     probs = torch.nn.functional.softmax(torch.tensor(logits), dim=-1).numpy()
 
     top3 = np.argsort(-probs, axis=1)[:, :3]  # Top 3 predictions
     match = top3 == labels[:, None]
 
-    # Compute MAP@3 manually
-    map3 = 0
-    for i in range(len(labels)):
-        if match[i, 0]:
-            map3 += 1.0
-        elif match[i, 1]:
-            map3 += 1.0 / 2
-        elif match[i, 2]:
-            map3 += 1.0 / 3
-    return {"map@3": map3 / len(labels)}
+    weights = np.array([1.0, 0.5, 1 / 3])
+    scores = np.sum(match * weights, axis=1)
+    return {"map@3": scores.mean()}
+
+
+def get_tokenizer(model_name):
+    return AutoTokenizer.from_pretrained(model_name)
+
+
+def get_sequence_classifier(model_name, num_labels):
+    return AutoModelForSequenceClassification.from_pretrained(
+        model_name, num_labels=num_labels
+    )
+
+
+def get_training_arguments(EPOCHS=10, train_batch_size=8, eval_batch_size=16):
+    return TrainingArguments(
+        output_dir="./output",
+        do_train=True,
+        do_eval=True,
+        eval_strategy="steps",
+        save_strategy="steps",  # no for no saving
+        num_train_epochs=EPOCHS,
+        per_device_train_batch_size=train_batch_size,
+        per_device_eval_batch_size=eval_batch_size,
+        learning_rate=5e-5,
+        logging_dir="./logs",
+        logging_steps=50,
+        save_steps=200,
+        eval_steps=200,
+        save_total_limit=1,
+        metric_for_best_model="map@3",
+        greater_is_better=True,
+        load_best_model_at_end=True,
+        report_to="none",
+        use_mps_device=True,  # Use MPS for Apple Silicon
+        # bf16=True,  # TRAIN WITH BF16 IF LOCAL GPU IS NEWER GPU
+        # fp16=True, # INFER WITH FP16 BECAUSE KAGGLE IS T4 GPU
+    )
+
+
+def get_trainer(
+    model,
+    tokenizer,
+    training_args,
+    train_ds,
+    val_ds,
+    compute_metrics=compute_map3,
+):
+    return Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=val_ds,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
